a025ef19c82a9c14c9f14e7da2a93a6c134e53d2
YuanBin <yuanbin.man@gmail.com>
2014-01-23 01:11:27 -0800

    Create graphwidget.h

---
+++ graphwidget.h
@@ -0,0 +1,27 @@
+#include <QtGui/QGraphicsView>
+
+class Node;
+
+class GraphWidget : public QGraphicsView
+{
+    Q_OBJECT
+
+public:
+    GraphWidget();
+
+    void itemMoved();
+
+protected:
+    void keyPressEvent(QKeyEvent *event);
+    void timerEvent(QTimerEvent *event);
+    void wheelEvent(QWheelEvent *event);
+    void drawBackground(QPainter *painter, const QRectF &rect);
+
+    void scaleView(qreal scaleFactor);
+
+private:
+    int timerId;
+    Node *centerNode;
+};
+
+#endif
